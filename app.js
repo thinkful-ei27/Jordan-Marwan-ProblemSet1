503eb61cb865b4932e705dce6c69f723005b1dd6
Jordan Heffernan <jheff2@gmail.com>
2018-12-03 11:53:30 -0800

    added year of birth

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 function createGreeting(name, age) {
-    return `Hi my name is ${name} and I'm ${age} years old`
+    return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`
 }
+
+let yearOfBirth = 2018 - age
 
 const greeting = createGreeting();
 console.log(greeting);
