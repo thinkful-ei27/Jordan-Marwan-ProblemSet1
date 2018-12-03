23c2241de89a30a307a5c7848f40a03350426a77
Marwan Alaloosi <manoalosi@gmail.com>
2018-12-03 13:58:12 -0600

    new function was added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 function createGreeting(name, age) {
-    return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`
+    const yob = getYearOfBirth()
+    return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yob}`
 }
 
+function getYearOfBirth(age){
 let yearOfBirth = 2018 - age
+return yearOfBirth;
+}
 
 const greeting = createGreeting();
 console.log(greeting);
