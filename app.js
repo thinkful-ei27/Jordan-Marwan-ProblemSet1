f716bf28a701df00d253e37df3b796fd2243399e
Jordan Heffernan <jheff2@gmail.com>
2018-12-03 12:41:02 -0800

    added typeOf error

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
+function getYearOfBirth(age){
+    let yearOfBirth = 2018 - age
+    return yearOfBirth;}
+
 function createGreeting(name, age) {
-
-    const yob = getYearOfBirth()
-
+    if (typeof age !== 'number') {
+        throw new TypeError ('Age must be a number')
+    } 
+    
     if(name === undefined || age === undefined) {
         throw new Error("Arguments not valid")
-    } else {
-    return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yob}`
-}
+    }
+
+    if (age < 0) {
+        throw new Error("Age can not be negative")} 
+    
+    const yob = getYearOfBirth(age)
+    return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yob}`;
 }
 
-function getYearOfBirth(age){
-let yearOfBirth = 2018 - age
-if (age < 0) {
-    throw new Error("Age can not be negative")} else {
-    return yearOfBirth;}
-}
+
+
 
 try {
-    const greeting = createGreeting();
+    const greeting = createGreeting("Rich", 18);
+    console.log(greeting);
 } catch (e) {
-    console.log(e)
+    console.error(e.message);
 } 
-console.log(greeting);
+
